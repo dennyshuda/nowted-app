4c662e347d239517bc2cbb5a26bd7f6f7d6c9539
Denny Setyawan Huda <dennshgit@gmail.com>
2023-01-29 17:46:21 +0700

    fix: link

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function Sidebar() {
   return (
-    <aside className="relative top-0 bottom-0 w-80 bg-gray-800">
+    <aside className="h-screen w-80 bg-gray-800">
       <nav className="py-5 px-2 text-white">
         <section className="text-center">
           <h1 className="uppercase text-2xl tracking-wider font-bold mb-10">
@@ -11,12 +11,14 @@ export function Sidebar() {
         </section>
         <ul className="space-y-5">
           <li>
-            <Link to="/">Active</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
             <Link to="/archived-notes">Archived</Link>
           </li>
-          <li>Add Notes</li>
+          <li>
+            <Link to="/add-notes">Add Notes</Link>
+          </li>
         </ul>
       </nav>
     </aside>
