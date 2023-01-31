2147d1fe6c500af4b3e4efee163b73c9f3ee343b
Denny Setyawan Huda <dennshgit@gmail.com>
2023-01-31 14:03:14 +0700

    feat: create form  add note

--- src/pages/AddNotes.tsx
+++ src/pages/AddNotes.tsx
@@ -1,11 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Container from "../components/Container";
+import React, { useContext, useState } from "react";
+import { NoteContext } from "../context/NoteContext";
 
 export function AddNotes() {
+  const { addNote } = useContext(NoteContext);
+  const [text, setText] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    addNote(text);
+    navigate("/");
+  };
+
   return (
     <div className="bg-gray-700 w-full">
       <Container>
         <h1>Add Notes</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="text-black"
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Add Todo"
+          />
+          <button>Add</button>
+        </form>
       </Container>
     </div>
   );
