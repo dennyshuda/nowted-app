6e91a2fddfc1c52fabac6e2326f6edb484a11d6a
Denny Setyawan Huda <dennshgit@gmail.com>
2023-01-31 14:02:44 +0700

    feat: render notes with context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,20 @@
+import { useContext } from "react";
 import Container from "../components/Container";
+import { NoteContext } from "../context/NoteContext";
 
 export function Home() {
+  const { notes, addNote } = useContext(NoteContext);
   return (
     <div className="bg-gray-700 w-full">
       <Container>
         <h1>Home Page</h1>
+        {notes.map((notes) => {
+          return (
+            <li key={notes.id}>
+              <p>{notes.title}</p>
+            </li>
+          );
+        })}
       </Container>
     </div>
   );
