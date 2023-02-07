cbae541a71d9ff5fc7e7db4c83010eb3ca86be1b
Denny Setyawan Huda <dennshgit@gmail.com>
2023-02-07 21:16:51 +0700

    feat: add auto size to form

--- src/pages/AddNotes.tsx
+++ src/pages/AddNotes.tsx
@@ -1,12 +1,17 @@
 import { Link, useNavigate } from "react-router-dom";
 import Container from "../components/Container";
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { NoteContext } from "../context/NoteContext";
+import useAutosizeTextArea from "../hooks/useAutosize";
 
 export function AddNotes() {
   const { addNote } = useContext(NoteContext);
   const [text, setText] = useState("");
   const navigate = useNavigate();
+
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  useAutosizeTextArea(textAreaRef.current, text);
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -18,15 +23,19 @@ export function AddNotes() {
     <div className="bg-gray-700 w-full">
       <Container>
         <h1>Add Notes</h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            className="text-black"
-            type="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            placeholder="Add Todo"
-          />
-          <button>Add</button>
+        <form className="border-2 rounded-md" onSubmit={handleSubmit}>
+          <div className="p-5 space-y-10">
+            <textarea
+              className="bg-transparent max-h-60 w-full focus: outline-none overflow-hidden resize-none"
+              value={text}
+              ref={textAreaRef}
+              onChange={(e) => setText(e.target.value)}
+              placeholder="Write your note here!!!"
+            />
+            <button className="bg-white text-gray-700 py-2 px-7 rounded-md ">
+              Add
+            </button>
+          </div>
         </form>
       </Container>
     </div>
