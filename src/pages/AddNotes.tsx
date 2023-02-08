774d136d8f32e13d6270ae3c46dedbea4053e026
Denny Setyawan Huda <dennshgit@gmail.com>
2023-02-08 08:54:56 +0700

    refactor: add form title and description

--- src/pages/AddNotes.tsx
+++ src/pages/AddNotes.tsx
@@ -3,19 +3,24 @@ import Container from "../components/Container";
 import React, { useContext, useRef, useState } from "react";
 import { NoteContext } from "../context/NoteContext";
 import useAutosizeTextArea from "../hooks/useAutosize";
+import { NoteType } from "../models/note";
 
 export function AddNotes() {
   const { addNote } = useContext(NoteContext);
-  const [text, setText] = useState("");
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   const navigate = useNavigate();
 
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
-  useAutosizeTextArea(textAreaRef.current, text);
+  useAutosizeTextArea(textAreaRef.current, description);
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    addNote(text);
+    // console.log({ title, description });
+    addNote(title, description);
     navigate("/");
   };
 
@@ -25,11 +30,19 @@ export function AddNotes() {
         <h1>Add Notes</h1>
         <form className="border-2 rounded-md" onSubmit={handleSubmit}>
           <div className="p-5 space-y-10">
+            <input
+              name="title"
+              className="bg-transparent focus:outline-none w-full"
+              type="text"
+              placeholder="Note Title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
             <textarea
+              name="description"
               className="bg-transparent max-h-60 w-full focus: outline-none overflow-hidden resize-none"
-              value={text}
+              value={description}
               ref={textAreaRef}
-              onChange={(e) => setText(e.target.value)}
+              onChange={(e) => setDescription(e.target.value)}
               placeholder="Write your note here!!!"
             />
             <button className="bg-white text-gray-700 py-2 px-7 rounded-md ">
