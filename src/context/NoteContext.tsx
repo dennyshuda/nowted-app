7df01465cf07e8e4ae0d54bab448e6ba043bb8ed
Denny Setyawan Huda <dennshgit@gmail.com>
2023-02-08 08:53:16 +0700

    feat: add uuid and delete function

--- src/context/NoteContext.tsx
+++ src/context/NoteContext.tsx
@@ -1,25 +1,42 @@
 import { ReactNode, createContext, useState } from "react";
 import { NoteContextType, NoteType } from "../models/note";
 import { NoteData } from "../data";
+import uuid from "react-uuid";
 
 interface ChildrenType {
   children: ReactNode;
 }
 
+interface inputUser {
+  title: string;
+  description: string;
+}
+
 export const NoteContext = createContext<NoteContextType>({
   notes: [],
   addNote: () => {},
+  deleteNote: () => {},
 });
 
 export function NoteContextProvider({ children }: ChildrenType) {
-  const [notes, Notes] = useState<NoteType[]>(NoteData);
+  const [notes, setNotes] = useState<NoteType[]>(NoteData);
 
-  const addNote = (title: string) => {
-    Notes([...notes, { id: Math.random(), title }]);
+  const addNote = (title: string, description: string) => {
+    const newNote = {
+      id: uuid(),
+      title: title,
+      description: description,
+      status: false,
+    };
+    setNotes([...notes, newNote]);
+  };
+
+  const deleteNote = (id: string) => {
+    setNotes(notes.filter((note) => note.id !== id));
   };
 
   return (
-    <NoteContext.Provider value={{ notes, addNote }}>
+    <NoteContext.Provider value={{ notes, addNote, deleteNote }}>
       {children}
     </NoteContext.Provider>
   );
